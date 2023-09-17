7c9e632e7d06ec09a308fbdea3cdc51c3a08b01c
Ihimbru-k <kanihims25@gmail.com>
2023-09-17 04:20:45 +0100

    contains all http packages and implementation of login and signup

---
+++ lib/login.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void login(String email, String password) async{
+
+    try{
+      Response response = await post(Uri.parse("https://reqres.in/api/register"),
+      body: {
+        'email' : email,
+        'password': password,
+      }
+      );
+
+      if(response.statusCode == 200){
+        var data = jsonDecode(response.body.toString());
+        print(data['token']);
+        print("login successful");
+      }else{
+        print("account doesn't exist");
+      }
+
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.2, horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+
+
+              const SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: (){
+                  login(emailController.text.toString(), passwordController.text.toString());
+                },
+
+
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 24.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: Text('Login'),
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+}
